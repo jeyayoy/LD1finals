b4e4ee094208a784c5c315f27c8bdaba3345b329
jeyayoy <cajayonjessellamarie@gmail.com>
2025-04-09 23:22:43 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+function validateLogin() {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const errorMsg = document.getElementById("error-message");
+
+  if (username === "admin123" && password === "admin123") {
+  
+    alert("Login successful!");
+    window.location.href = "cafe.html"; 
+    return false; 
+  } else {
+    errorMsg.textContent = "Invalid username or password!";
+    return false; 
+  }
+}
